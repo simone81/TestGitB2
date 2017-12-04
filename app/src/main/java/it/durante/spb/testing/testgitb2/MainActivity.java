587d51c1b14b33a82c673b9691f674ce9e0bd70d
SCappello <scappello@durante.it>
2017-12-04 12:10:41 +0100

    sviluppo nuove funzionalità. Campi in homePage con rel corrente e nome

--- app/src/main/java/it/durante/spb/testing/testgitb2/MainActivity.java
+++ app/src/main/java/it/durante/spb/testing/testgitb2/MainActivity.java
@@ -5,14 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    // inizio FIX rel 1.0.1
+    // inizio sviluppo nuove funzionalità 2.0.0
+
+    private static final String currentRelease = "2.0.0";
+
+    TextView txtRelease;
+    TextView txtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("FIX 1.0.1", null).show();
             }
         });
+
+
+        txtRelease = (TextView) findViewById(R.id.txtRelease);
+        txtRelease.setText("CURRENT RELEASE: " + currentRelease);
+
+        // nuovo campo rel 2.0.0
+        txtNome = (TextView) findViewById(R.id.txtNome);
     }
 
     @Override
